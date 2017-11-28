2dce62e010d6094ea2f407e786b49cce6e4633de
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2017-11-28 12:12:48 +1100

    Add key to todo item component

--- src/App.js
+++ src/App.js
@@ -3,13 +3,27 @@ import React, { Component } from 'react'
 import './App.css'
 import TodoItem from './component/TodoItem'
 
+// Data
+const items = [
+  { description: 'First', completed: true },
+  { description: 'Second', completed: true },
+  { description: 'Third', completed: false },
+]
+
+// UI
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <TodoItem description='First' completed />
-        <TodoItem description='Second' completed />
-        <TodoItem description='Third' />
+      {
+        items.map( (item, index) => (
+          <TodoItem
+            key={ index }
+            description={ item.description }
+            completed={ item.completed }
+          />
+        ))
+      }
       </div>
     )
   }
