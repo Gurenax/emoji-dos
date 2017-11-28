6a8555ca15443004f0f70c18326ec2dd1934ad5b
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2017-11-28 12:55:16 +1100

    Add state for TodoItem component

--- src/App.js
+++ src/App.js
@@ -3,16 +3,74 @@ import React, { Component } from 'react'
 import './App.css'
 import TodoItem from './component/TodoItem'
 
-// Data
-const items = [
-  { description: 'First', completed: true },
-  { description: 'Second', completed: true },
-  { description: 'Third', completed: false },
-]
-
-// UI
+// UI (view/controller)
 class App extends Component {
+  state = {
+    items: [
+      { description: 'First', completed: false },
+      { description: 'Second', completed: true },
+      { description: 'Third', completed: false },
+      { description: 'Fourth', completed: true },
+    ]
+  }
+
+  // onToggleItemAtIndex = (index) => {
+  //   // Data (model)
+  //   this.setState( (prevState) => {
+  //     // Get curent items
+  //     const items = prevState.items
+  //     // Find the item with the particular 'index'
+  //     const item = items[index]
+  //     // Toggle completed: f -> t, t -> f
+  //     item.completed = !item.completed
+  //     // Return the changes we have
+  //     return {
+  //       items: items
+  //     }
+  //   })
+  // }
+  onToggleItemAtIndex = (index) => {
+    // Data (model)
+    this.setState( (prevState) => {
+      const beforeItems = prevState.items
+      const afterItems = beforeItems.map( (item, currentIndex) => {
+        // WHen we have found the index of the item to change
+        if (currentIndex === index) {
+          // Long way:
+          // const copy = Object.assign(
+          //   {}, // Start with a new blank object
+          //   item, // Copy key-value pairs form existing items
+          //   { completed: !item.completed } // Override the completed to be the opposite of before
+          // )
+          // Modern way:
+          const copy = {
+            ...item, // Copy the key-value pairs in item
+            completed: !item.completed  // Override the completed to be the opposite of before
+          }
+          return copy
+        }
+        // No changes to make to this particular item
+        else {
+          return item
+        }
+      })
+      
+      // Get curent items
+      const items = prevState.items
+      // Find the item with the particular 'index'
+      const item = items[index]
+      // Toggle completed: f -> t, t -> f
+      item.completed = !item.completed
+      // Return the changes we have
+      return {
+        items: items
+      }
+    })
+  }
+
   render() {
+    const items = this.state.items
+
     return (
       <div className="App">
       {
@@ -21,6 +79,11 @@ class App extends Component {
             key={ index }
             description={ item.description }
             completed={ item.completed }
+            onToggleCompleted={ () => {
+                console.log('TodoItem onToggleCompleted received', index)
+                this.onToggleItemAtIndex(index)
+              }
+            }
           />
         ))
       }
