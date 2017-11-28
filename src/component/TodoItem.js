344f8337022711ed92dd9d7b89ab1ca9d2c4ba83
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2017-11-28 14:23:07 +1100

    Change ToDo item checkbox to emoji buttons

--- src/component/TodoItem.js
+++ src/component/TodoItem.js
@@ -5,21 +5,22 @@ const TodoItem = ({
   completed = false, // if not passed, default to 'false'
   onToggleCompleted
 }) => {
+  function emoji(completed) {
+    return completed ? '✅' : '❎'
+  }
   return (
     <label>
-      <input
-        type='checkbox'
-        checked={ completed }
-        onChange={
-          (event) => { // Event listener
-            console.log('Clicked checkbox', description)
-            // When user clicks checkbox
-            // Notify the powers above that the
-            // concept of completed has changed
-            onToggleCompleted()
-          }
-        }
-      />
+      <button onClick={
+        (event) => { // Event listener
+          console.log('Clicked checkbox', description)
+          // When user clicks checkbox
+          // Notify the powers above that the
+          // concept of completed has changed
+          onToggleCompleted()
+      }} >
+      { emoji(completed) }
+      </button>
+      
       { description }
     </label>
   )
